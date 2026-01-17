edcafd2f91bfac72c3d1d2de5e054f1d71ed28c2
Richie Bendall <richiebendall@gmail.com>
2026-01-17 21:56:23 +1300

    Normalize indentation

--- templates/uv-cuda/flake.nix
+++ templates/uv-cuda/flake.nix
@@ -1,34 +1,34 @@
 {
-  inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
-  };
+	inputs = {
+		nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+	};
 
-  outputs =
-    { nixpkgs, ... }:
-    let
-      inherit (nixpkgs) lib;
-      forAllSystems = lib.genAttrs lib.systems.flakeExposed;
-    in
-    {
-      devShells = forAllSystems (
-        system:
-        let
-          pkgs = import nixpkgs {
-            inherit system;
-            config = {
-            	allowUnfree = true;
-            };
-          };
-        in
-        {
-          default = pkgs.mkShell {
-            packages = with pkgs; [
-              python3
-              uv
-              cudatoolkit
-            ];
+	outputs =
+		{ nixpkgs, ... }:
+		let
+			inherit (nixpkgs) lib;
+			forAllSystems = lib.genAttrs lib.systems.flakeExposed;
+		in
+		{
+			devShells = forAllSystems (
+				system:
+				let
+					pkgs = import nixpkgs {
+						inherit system;
+						config = {
+							allowUnfree = true;
+						};
+					};
+				in
+				{
+					default = pkgs.mkShell {
+						packages = with pkgs; [
+							python3
+							uv
+							cudatoolkit
+						];
 
-            env = lib.optionalAttrs pkgs.stdenv.isLinux {
+						env = lib.optionalAttrs pkgs.stdenv.isLinux {
 								LD_LIBRARY_PATH = with pkgs; lib.makeLibraryPath (
 									[
 										# CUDA shared libraries
@@ -43,15 +43,15 @@
 									# Setting LD_LIBRARY_PATH makes the dynamic library loader aware of libraries without using RPATH for lookup.
 									++ pythonManylinuxPackages.manylinux1
 								);
-            };
+						};
 
-            shellHook = ''
-              unset PYTHONPATH
-              uv sync
-              . .venv/bin/activate
-            '';
-          };
-        }
-      );
-    };
+						shellHook = ''
+							unset PYTHONPATH
+							uv sync
+							. .venv/bin/activate
+						'';
+					};
+				}
+			);
+		};
 }
